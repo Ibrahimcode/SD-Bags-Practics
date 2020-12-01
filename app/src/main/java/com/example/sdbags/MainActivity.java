345d4b609fd4dd854d7396c5d667876dde3169f6
Ibrahimcode <ibrahamacademic@gmail.com>
2020-12-01 20:56:10 +0000

    The welcome login and login activity.

--- app/src/main/java/com/example/sdbags/MainActivity.java
+++ app/src/main/java/com/example/sdbags/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.sdbags;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button joinNowButton, loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        joinNowButton = (Button) findViewById(R.id.main_join_now_btn);
+        loginButton = (Button) findViewById(R.id.main_login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
